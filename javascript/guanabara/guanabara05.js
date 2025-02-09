eefc1ccde5252ed6bc6eba453e42b3f53d4ff37c
Rebeca Carneiro <rebecacarneiropaulaoficial@gmail.com>
2025-02-09 19:02:05 -0300

    guanabara06

---
+++ javascript/guanabara/guanabara05.js
@@ -0,0 +1,12 @@
+var c = 1
+while (c <=6){
+    console.log(`passo ${c}`)
+    c++
+}
+
+//fazer e depois verificar:
+var num = 1
+do{
+    console.log(num)
+    num++
+}while (num <= 3)
---
+++ javascript/guanabara/guanabara06.js
@@ -0,0 +1,4 @@
+//for (inicio; teste; incremento){}
+for (c = 1; c<=10; c++){
+    console.log(c)
+}
