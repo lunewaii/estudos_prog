6f40a8197d0009073c9d27ffbffb122f36851ccd
Rebeca Carneiro <rebecacarneiropaulaoficial@gmail.com>
2025-02-16 21:44:21 -0300

    mudei pq tava aparecendo preterido >:(

--- javascript/brazilian dev/ternario.js
+++ javascript/brazilian dev/ternario.js
@@ -2,5 +2,5 @@
 //condição ? sim : não
 //exemplo de uso
 var idade = 15;
-var status = (idade >= 18) ? 'adulto' : 'menor';
-console.log(status); // adulto
+var id = (idade >= 18) ? 'adulto' : 'menor';
+console.log(id); // adulto
