62e9c5b78c6820bf7046d908784a1c4f4702ba43
rebeca <rebecacarneiropaulaoficial@gmail.com>
2025-03-19 18:02:43 -0300

    datas

---
+++ javascript/datas.js
@@ -0,0 +1,30 @@
+//como saber a data atual:
+var data = new Date();
+console.log(data);
+
+//como saber o ano atual:
+console.log(data.getFullYear());
+
+//como saber o mês atual:
+console.log(data.getMonth());
+
+//como saber o dia atual:
+console.log(data.getDate());
+
+//como saber o dia da semana atual:
+console.log(data.getDay());
+
+//como saber a hora atual:
+console.log(data.getHours());
+
+//como saber os minutos atuais:
+console.log(data.getMinutes());
+
+//como saber os segundos atuais:
+console.log(data.getSeconds());
+
+//como saber os milisegundos atuais:
+console.log(data.getMilliseconds());
+
+//como saber o timestamp atual:
+console.log(data.getTime());
