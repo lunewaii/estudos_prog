2f0ad04f7e9782b654b7397243ff2e6b79700421
rebeca <rebecacarneiropaulaoficial@gmail.com>
2025-03-19 18:15:34 -0300

    mais datas

--- javascript/datas.js
+++ javascript/datas.js
@@ -1,30 +1,34 @@
 //como saber a data atual:
-var data = new Date();
-console.log(data);
+// var data = new Date();
+// console.log(data);
 
-//como saber o ano atual:
-console.log(data.getFullYear());
+// //como saber o ano atual:
+// console.log(data.getFullYear());
 
-//como saber o mês atual:
-console.log(data.getMonth());
+// //como saber o mês atual:
+// console.log(data.getMonth());
 
-//como saber o dia atual:
-console.log(data.getDate());
+// //como saber o dia atual:
+// console.log(data.getDate());
 
-//como saber o dia da semana atual:
-console.log(data.getDay());
+// //como saber o dia da semana atual:
+// console.log(data.getDay());
 
-//como saber a hora atual:
-console.log(data.getHours());
+// //como saber a hora atual:
+// console.log(data.getHours());
 
-//como saber os minutos atuais:
-console.log(data.getMinutes());
+// //como saber os minutos atuais:
+// console.log(data.getMinutes());
 
-//como saber os segundos atuais:
-console.log(data.getSeconds());
+// //como saber os segundos atuais:
+// console.log(data.getSeconds());
 
-//como saber os milisegundos atuais:
-console.log(data.getMilliseconds());
+// //como saber os milisegundos atuais:
+// console.log(data.getMilliseconds());
 
-//como saber o timestamp atual:
-console.log(data.getTime());
+// //como saber o timestamp atual:
+// console.log(data.getTime());
+
+//posso também passar uma data específica:
+var date = new Date("03/19/2025");
+console.log(date.getDate());
