f85e18f491d78b58fda84a9a4e264b4712cd27b6
Rebeca Carneiro <rebecacarneiropaulaoficial@gmail.com>
2025-03-30 13:19:47 -0300

    parametro opcional e mudanca pra ver o nodemon funcionando

--- node/rotasParametros.js
+++ node/rotasParametros.js
@@ -6,9 +6,10 @@ app.get("/", function(req, res){
 });
 
 //so posso mandar o send uma vez!
-app.get("/oi/:nome", function(req, res){
+//o ? deixa o parametro como opcional
+app.get("/oi/:nome?", function(req, res){
     const nome = req.params.nome; //pega o parametro da url
-    res.send('Oi ' + nome + '!');
+    res.send('Oi ' + nome + ' :)');
 })
 
 
