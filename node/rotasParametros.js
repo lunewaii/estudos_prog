5a1c8c4a2c543381db745e6503fd9153cee120c1
Rebeca Carneiro <rebecacarneiropaulaoficial@gmail.com>
2025-03-30 13:14:40 -0300

    parametros

---
+++ node/rotasParametros.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get("/", function(req, res){
+    res.send('pagina carregada com sucesso!');
+});
+
+//so posso mandar o send uma vez!
+app.get("/oi/:nome", function(req, res){
+    const nome = req.params.nome; //pega o parametro da url
+    res.send('Oi ' + nome + '!');
+})
+
+
+app.listen(3000, function(){
+    console.log('Servidor rodando');
+})
