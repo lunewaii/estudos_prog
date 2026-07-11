06b5cb0d77c9273dbb8c4106d06ebd0e4c421be1
Rebeca Carneiro <rebecacarneiropaulaoficial@gmail.com>
2026-07-11 19:01:27 -0300

    verifica se usuário e senha estão corretos

--- python/flask-auth/app.py
+++ python/flask-auth/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
@@ -12,6 +12,10 @@ db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
@@ -21,9 +25,11 @@ def login():
     if username and password:
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
             return jsonify({'message': 'Sucesso no login'}), 200
-    else:
-        return jsonify({'message': 'Credenciais inválidas'}), 401
+    
+    return jsonify({'message': 'Credenciais inválidas'}), 401
 
 @app.route('/hi', methods=['GET'])
 def hi():
